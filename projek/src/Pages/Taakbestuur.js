a24db1dac0e1b50bc0d3015d01a3b7f6a88f88e8
Jannas133 <duvenhagejan133@gmail.com>
2024-08-12 12:22:44 +0200

    taak

--- projek/src/Pages/Taakbestuur.js
+++ projek/src/Pages/Taakbestuur.js
@@ -3,16 +3,18 @@ import './Styles/taakbestuur.css';
 
 function TaskForm ({newTask}) {
     const [taskName, setName] = useState('');
+    const [taskProject, setProject] = useState('');
     const [taskDescription, setDescription] = useState('');
     const [taskStart, setStart] = useState('');
     const [taskEnd, setEnd] = useState('');
     const handleAdd = (e) => {
         e.preventDefault();
-        newTask({taskName, taskDescription, taskStart, taskEnd, tStatus: 1});
+        newTask({taskName, taskProject, taskDescription, taskStart, taskEnd, tStatus : 'To Do'});
         setName('');
+        setProject('');
         setDescription('');
-        setStart('');
-        setEnd('To Do');
+        setStart('Start');
+        setEnd('End');
     };
     return (
         <form className='add' onSubmit={handleAdd}>
@@ -21,6 +23,13 @@ function TaskForm ({newTask}) {
                 placeholder="Taak Naam"
                 value={taskName}
                 onChange={(e) => setName(e.target.value)}
+                required
+            />
+            <input 
+                type="text"
+                placeholder="Projek Naam"
+                value={taskProject}
+                onChange={(e) => setProject(e.target.value)}
                 required
             />
             <input
@@ -52,39 +61,45 @@ function TaskForm ({newTask}) {
 function TaskItem ({task, editTask, removeTask, handleStatus}) {
     const [edit, setEdit] = useState(false);
     const [taskName, setName] = useState(task.taskName);
+    const [taskProject, setProject] = useState(task.taskProject);
     const [taskDescription, setDescription] = useState(task.taskDescription);
     const [taskStart, setStart] = useState(task.taskStart);
     const [taskEnd, setEnd] = useState(task.taskEnd);
     const handleEdit = () => {
-        editTask(task.id, {...task, taskName, taskDescription, taskStart, taskEnd});
+        editTask(task.id, {...task, taskName, taskProject, taskDescription, taskStart, taskEnd});
         setEdit(false);
     };
     return (
         <div>
             {edit ? (
                 <div>
-                <input 
-                    type="text"
-                    value={taskName}
-                    onChange={(e) => setName(e.target.value)}
-                />
-                <input 
-                    type="text"
-                    value={taskDescription}
-                    onChange={(e) => setDescription(e.target.value)}
-                />
-                <input 
-                    type="date"
-                    value={taskStart}
-                    onChange={(e) => setStart(e.target.value)}
-                />
-                <input 
-                    type="date"
-                    value={taskEnd}
-                    onChange={(e) => setEnd(e.target.value)}
-                />
-                <button onClick={handleEdit}>Save</button>
-                <button onClick={() => setEdit(false)}>Cancel</button>
+                    <input 
+                        type="text"
+                        value={taskName}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    <input 
+                        type="text"
+                        value={taskProject}
+                        onChange={(e) => setProject(e.target.value)}
+                    />
+                    <input 
+                        type="text"
+                        value={taskDescription}
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
+                    <input 
+                        type="date"
+                        value={taskStart}
+                        onChange={(e) => setStart(e.target.value)}
+                    />
+                    <input 
+                        type="date"
+                        value={taskEnd}
+                        onChange={(e) => setEnd(e.target.value)}
+                    />
+                    <button onClick={handleEdit}>Save</button>
+                    <button onClick={() => setEdit(false)}>Cancel</button>
                 </div>
             ) :(
                 <div>
@@ -130,6 +145,8 @@ const TaakBestuur = () => {
                     <div className="underline"></div>
                 </div>
                 <div className='addtask'>
+                    <h1>Nuwe Taak</h1>
+                    <div className="underline"></div>
                     <TaskForm newTask={newTask}/>
                 </div>
                 <div className='listtask'>
